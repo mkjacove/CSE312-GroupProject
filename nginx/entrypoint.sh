0b0ac78fc2f20a894173e0e2ddabacca5f61283b
Matt <mattjacovelli@outlook.com>
2025-04-19 16:44:04 -0400

    fixed logging issue with websockets, organized log files to remake themselves on container reset

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-echo "[INIT] Clearing access.log and error.log..."
-: > /var/www/logs/access.log
-: > /var/www/logs/error.log
+echo "[INIT] Clearing old log files..."
 
+# Remove any duplicated or rotated logs
+rm -f /var/www/logs/*.log*
+
+# Re-create empty logs
+touch /var/www/logs/access.log /var/www/logs/error.log
+chmod 666 /var/www/logs/*.log
+
+# Start Nginx
 exec nginx -g "daemon off;"
